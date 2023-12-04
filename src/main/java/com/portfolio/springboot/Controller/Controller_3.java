3a20c878a08a3f1d39c9feb6ee74219a325a7eb8
kang0330 <shflato9070@gmail.com>
2023-12-04 12:17:00 +0900

    123
    123

--- src/main/java/com/portfolio/springboot/Controller/Controller_3.java
+++ src/main/java/com/portfolio/springboot/Controller/Controller_3.java
@@ -4,13 +4,16 @@ package com.portfolio.springboot.Controller;
 import com.portfolio.springboot.entity.NoticeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //  이강희
 @Controller
 public class Controller_3 {
-    @Autowired
-    private NoticeRepository noticeRepository;
+    @GetMapping("/admin_notice_add")
+    public String adminNoticeAdd(RequestParam String )
 
 
 
