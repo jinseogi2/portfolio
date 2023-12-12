6b180d0b500994228528b6dbf113999196e4c96a
KHpark9606 <kihyun1996.park@gmail.com>
2023-12-12 14:19:24 +0900

    basket1
    dd

---
+++ src/main/java/com/portfolio/springboot/dto/BasketDto.java
@@ -0,0 +1,39 @@
+﻿package com.portfolio.springboot.dto;
+
+import com.portfolio.springboot.entity.CartEntity;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasketDto {
+    private long cartNo;
+    private String cartName;
+    private String cartImgUrl1;
+    private String cartPrice;
+    private int cartOptionPrice1;
+    private int cartOptionPrice2;
+    private int cartOptionPrice3;
+    private long cartCount;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime cartDatetime;
+
+    public static BasketDto toCartDto(CartEntity entity){
+        return BasketDto.builder()
+                .cartNo(entity.getCartNo())
+                .itemName(entity.getItemName())
+                .itemImageUrl(entity.getItemImageUrl())
+                .itemPrice(entity.getItemPrice())
+                .cartitemOptionprice1(entity.itemOptionprice1())
+                .cartitemOptionprice2(entity.itemOptionprice2())
+                .cartitemOptionprice3(entity.itemOptionprice3())
+                .cartDatetime(entity.getCartDatetime())
+                .build();
+    }
+}
---
+++ src/main/java/com/portfolio/springboot/entity/BasketEntity.java
@@ -0,0 +1,57 @@
+﻿package com.portfolio.springboot.entity;
+
+import com.portfolio.springboot.dto.BasketDto;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="basket")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasketEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_no")
+    private long cartNo;
+    @Column(name = "cart_name")
+    private String cartName;
+    @Column(name = "cart_image_url_1")
+    private String cartImageUrl;
+    @Column(name = "cart_price")
+    private long cartPrice;
+    @Column(name = "cart_item_amount")
+    private long cartItemAmount;
+    @Column(name = "cart_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime cartDate;
+
+    public static BasketEntity BasketEntity(BasketDto dto) {
+        return BasketEntity.builder()
+                .cartNo(dto.getCartNo())
+                .itemName(dto.getCartName())
+                .cartImageUrl1(dto.getCartImageurl1()) // 수정된 부분
+                .itemPrice(dto.getItemPrice())
+                .cartItemAmount(dto.getCartItemAmount())
+                .cartDate(dto.getCartDate())
+                .build();
+    }
+
+    public static BasketEntity toEntity(BasketDto dto) {
+        return BasketEntity.builder()
+                .cartNo(dto.getCartNo())
+                .cartCode(dto.getCartCode())
+                .itemCode(dto.getItemCode())
+                .itemName(dto.getCartName())
+                .itemImageUrl(dto.getItemImageUrl()) // 수정된 부분
+                .itemPrice(dto.getItemPrice())
+                .cartItemAmount(dto.getCartItemAmount())
+                .cartDate(dto.getCartDate())
+                .build();
+    }
+}
